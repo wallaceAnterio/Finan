f8047826cec247b5d96e39262e75048a57735eb1
wallaceAnterio <wallacesanterio@gmail.com>
2024-03-01 10:24:37 +0000

    improving category form template

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -4,18 +4,9 @@ import { NgModule } from '@angular/core';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryListComponent } from './category-list/category-list.component';
-import { ModalConfirmComponent } from './shared/modal-confirm/modal-confirm.component';
-
 
 @NgModule({
-  declarations: [
-    CategoryListComponent,
-    CategoryFormComponent,
-    ModalConfirmComponent
-  ],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule,
-  ]
+  declarations: [CategoryListComponent, CategoryFormComponent],
+  imports: [CommonModule, CategoriesRoutingModule],
 })
-export class CategoriesModule { }
+export class CategoriesModule {}
