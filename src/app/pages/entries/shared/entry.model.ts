9a017edf3f70853aeded05457a4679d94e45e246
wallaceAnterio <wallacesanterio@gmail.com>
2024-03-04 17:01:38 +0000

    setting up Entriesmodule

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,22 @@
+import { Category } from '../../categories/shared/category.model';
+
+export class Entry {
+  id?: number;
+  name?: string;
+  description?: string;
+  type?: string;
+  amount?: string;
+  date?: string;
+  paid?: boolean;
+  categoryId?: number;
+  category?: Category;
+
+  static types = {
+    expense: 'Despesa',
+    renevue: 'Receita',
+  };
+
+  get paidText(): string {
+    return this.paid ? 'Pago' : 'Pendente';
+  }
+}
